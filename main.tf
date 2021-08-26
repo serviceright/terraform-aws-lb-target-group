f3d2e5d1dd55f83c2a30b1354115be80448417bd
Soren Martius <soeren.martius@gmail.com>
2021-08-26 10:24:29 +0300

    feat: add support for aws_lb_target_group resource

--- main.tf
+++ main.tf
@@ -1,5 +1,62 @@
-# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-# THIS IS A UPPERCASE MAIN HEADLINE
-# And it continues with some lowercase information about the module
-# We might add more than one line for additional information
-# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+locals {
+  name                               = var.name
+  name_prefix                        = var.name == null ? var.name_prefix : null
+  port                               = var.target_type != "lambda" ? var.port : null
+  protocol                           = var.target_type != "lambda" ? var.protocol : null
+  protocol_version                   = can(regex("^HTTPS?$", var.protocol)) ? var.protocol_version : null
+  lambda_multi_value_headers_enabled = var.target_type == "lambda" ? var.lambda_multi_value_headers_enabled : null
+  default_preserve_client_ip         = var.protocol != null ? (contains(["TCP", "TLS"], var.protocol) ? false : contains(["UDP", "TCP_UDP"], var.protocol) ? true : null) : null
+  preserve_client_ip                 = var.preserve_client_ip != null ? var.preserve_client_ip : local.default_preserve_client_ip
+  vpc_id                             = can(regex("^(instance|ip)$", var.target_type)) ? var.vpc_id : null
+}
+
+resource "aws_lb_target_group" "target_group" {
+  count = var.module_enabled ? 1 : 0
+
+  name             = local.name
+  name_prefix      = local.name_prefix
+  port             = local.port
+  protocol         = local.protocol
+  protocol_version = local.protocol_version
+  target_type      = var.target_type
+
+  vpc_id = local.vpc_id
+
+  deregistration_delay               = var.deregistration_delay
+  lambda_multi_value_headers_enabled = local.lambda_multi_value_headers_enabled
+  load_balancing_algorithm_type      = var.load_balancing_algorithm_type
+  preserve_client_ip                 = local.preserve_client_ip
+
+  dynamic "health_check" {
+    for_each = var.health_check != null ? [var.health_check] : []
+
+    content {
+      enabled = try(health_check.value.enabled, true)
+
+      healthy_threshold   = try(health_check.value.healthy_threshold, null)
+      interval            = try(health_check.value.interval, null)
+      matcher             = try(health_check.value.matcher, null)
+      path                = try(health_check.value.path, null)
+      port                = try(health_check.value.port, null)
+      protocol            = try(health_check.value.protocol, null)
+      timeout             = try(health_check.value.timeout, null)
+      unhealthy_threshold = try(health_check.value.unhealthy_threshold, null)
+    }
+  }
+
+  dynamic "stickiness" {
+    for_each = var.stickiness != null ? [var.stickiness] : []
+
+    content {
+      enabled = try(stickiness.value.enabled, true)
+
+      type            = stickiness.value.type
+      cookie_name     = try(stickiness.value.cookie_name, null)
+      cookie_duration = try(stickiness.value.cookie_duration, null)
+    }
+  }
+
+  tags = merge(var.module_tags, var.tags)
+
+  depends_on = [var.module_depends_on]
+}
